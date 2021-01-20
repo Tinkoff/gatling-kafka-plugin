81a0335db3c9983b2eebcd44ec819ab59955f95e
Max <maksimall89@gmail.com>
2021-01-20 16:03:53 +0400

    fix problem with slf4j (#10)
    * fix exclude slf4j-api
    
    Co-authored-by: m.rogozhnikov <1qaz2wsx3E>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,8 +11,8 @@ object Dependencies {
       .exclude("org.slf4j", "slf4j-api"))
 
   lazy val avro4s: Seq[ModuleID] = Seq(
-    ("com.sksamuel.avro4s" %% "avro4s-core" % "1.9.0" % "provided")
-      .exclude("org.slf4j", "slf4j-api"))
+    "com.sksamuel.avro4s" %% "avro4s-core"
+  ).map(_% "1.9.0" % "provided")
 
   lazy val gradleKafka: Seq[ModuleID] = Seq(
     ("com.github.imflog" % "kafka-schema-registry-gradle-plugin" % "0.9.0")
@@ -23,16 +23,16 @@ object Dependencies {
       .exclude("org.slf4j", "slf4j-api"))
 
   lazy val avroPlugin: Seq[ModuleID] = Seq(
-    ("org.apache.avro" % "avro-maven-plugin" % "1.09.0")
-      .exclude("org.slf4j", "slf4j-api"))
+    "org.apache.avro" % "avro-maven-plugin"
+  ).map(_ % "1.09.0" )
 
   lazy val gradle_logging: Seq[ModuleID] = Seq(
     ("org.gradle" % "gradle-logging" % "4.3" % "runtime")
       .exclude("org.slf4j", "slf4j-api"))
 
   lazy val gradle_base: Seq[ModuleID] = Seq(
-    "org.gradle" % "gradle-base-services"
-  ).map(_ % "4.3-rc-4" % "runtime")
+    ("org.gradle" % "gradle-base-services" % "4.3-rc-4" % "runtime")
+      .exclude("org.slf4j", "slf4j-api"))
 
   lazy val gradle_files: Seq[ModuleID] = Seq(
     "org.gradle" % "gradle-core",
@@ -41,11 +41,7 @@ object Dependencies {
   ).map(_ % "6.1.1" % "runtime")
 
   lazy val ant: Seq[ModuleID] = Seq(
-    "org.apache.ant" % "ant"
-  ).map(_ % "1.8.2")
-
-//  lazy val net_jcip: Seq[ModuleID] = Seq(
-//    "net.jcip" % "annotations"
-//  ).map(_ % "1.0")
-
+    ("org.apache.ant" % "ant" % "1.8.2")
+      .exclude("org.slf4j", "slf4j-api"))
+  
 }
