c3cde2d416639983d7919ce4fa99e124b5425dc7
m.rogozhnikov <1qaz2wsx3E>
2021-01-22 13:32:50 +0300

    add exclude jboss

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,49 +8,56 @@ object Dependencies {
 
   lazy val kafka: Seq[ModuleID] = Seq(
     ("org.apache.kafka" % "kafka-clients" % "2.2.2")
-      .exclude("org.slf4j", "slf4j-api"))
+      .exclude("org.slf4j", "slf4j-api")
+      .exclude("org.jboss.logging", "jboss-logging"))
 
   lazy val avro4s: Seq[ModuleID] = Seq(
     ("com.sksamuel.avro4s" %% "avro4s-core" % "1.9.0" % "provided")
-      .exclude("org.slf4j", "slf4j-api"))
+      .exclude("org.slf4j", "slf4j-api")
+      .exclude("org.jboss.logging", "jboss-logging"))
 
   lazy val gradleKafka: Seq[ModuleID] = Seq(
     ("com.github.imflog" % "kafka-schema-registry-gradle-plugin" % "0.9.0")
-      .exclude("org.slf4j", "slf4j-api"))
+      .exclude("org.slf4j", "slf4j-api")
+      .exclude("org.jboss.logging", "jboss-logging"))
 
   lazy val gradleAvro: Seq[ModuleID] = Seq(
-    ("com.commercehub.gradle.plugin" % "gradle-avro-plugin" % "0.20.0")
-      .exclude("org.slf4j", "slf4j-api"))
+    ("com.commercehub.gradle.plugin" % "gradle-avro-plugin" % "0.22.0")
+      .exclude("org.slf4j", "slf4j-api")
+      .exclude("org.jboss.logging", "jboss-logging"))
 
   lazy val avroPlugin: Seq[ModuleID] = Seq(
     ("org.apache.avro" % "avro-maven-plugin" % "1.09.0")
-      .exclude("org.slf4j", "slf4j-api"))
+      .exclude("org.slf4j", "slf4j-api")
+      .exclude("org.jboss.logging", "jboss-logging"))
 
   lazy val gradle_logging: Seq[ModuleID] = Seq(
     ("org.gradle" % "gradle-logging" % "4.3" % "runtime")
-      .exclude("org.slf4j", "slf4j-api"))
+      .exclude("org.slf4j", "slf4j-api")
+      .exclude("org.jboss.logging", "jboss-logging"))
 
   lazy val gradle_base: Seq[ModuleID] = Seq(
     ("org.gradle" % "gradle-base-services" % "4.3-rc-4" % "runtime")
-      .exclude("org.slf4j", "slf4j-api"))
+      .exclude("org.slf4j", "slf4j-api")
+      .exclude("org.jboss.logging", "jboss-logging"))
 
   lazy val gradle_files_gradle_core: Seq[ModuleID] = Seq(
     ("org.gradle" % "gradle-core" % "6.1.1" % "runtime")
-      .exclude("org.slf4j", "slf4j-api"))
+    .exclude("org.slf4j", "slf4j-api")
+    .exclude("org.jboss.logging", "jboss-logging"))
+
   lazy val gradle_files_gradle_messaging: Seq[ModuleID] = Seq(
     ("org.gradle" % "gradle-messaging" % "6.1.1" % "runtime")
-      .exclude("org.slf4j", "slf4j-api"))
+      .exclude("org.slf4j", "slf4j-api")
+      .exclude("org.jboss.logging", "jboss-logging"))
+
   lazy val gradle_files_gradle_native: Seq[ModuleID] = Seq(
     ("org.gradle" % "gradle-native" % "6.1.1" % "runtime")
-      .exclude("org.slf4j", "slf4j-api"))
-
-//  lazy val gradle_files: Seq[ModuleID] = Seq(
-//    "org.gradle" % "",
-//    "org.gradle" % "",
-//    "org.gradle" % ""
-//  ).map(_ % "6.1.1" % "runtime")
+      .exclude("org.slf4j", "slf4j-api")
+      .exclude("org.jboss.logging", "jboss-logging"))
 
   lazy val ant: Seq[ModuleID] = Seq(
     ("org.apache.ant" % "ant" % "1.8.2")
-      .exclude("org.slf4j", "slf4j-api"))
+      .exclude("org.slf4j", "slf4j-api")
+      .exclude("org.jboss.logging", "jboss-logging"))
 }
