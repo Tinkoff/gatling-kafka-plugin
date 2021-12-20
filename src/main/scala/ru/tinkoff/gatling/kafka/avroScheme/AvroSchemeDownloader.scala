63ecf8337dec621bd0ff46dc95bd1945c5f31d6f
i.akhaltsev <i.akhaltsev@tinkoff.ru>
2021-12-20 21:46:44 +0300

    chore: up version

--- src/main/scala/ru/tinkoff/gatling/kafka/avroScheme/AvroSchemeDownloader.scala
+++ src/main/scala/ru/tinkoff/gatling/kafka/avroScheme/AvroSchemeDownloader.scala
@@ -1,5 +1,8 @@
 package ru.tinkoff.gatling.kafka.avroScheme
 
+import com.github.imflog.schema.registry.RegistryClientWrapper
+import com.github.imflog.schema.registry.tasks.download.{DownloadSubject, DownloadTaskAction}
+
 import java.io.File
 import java.util
 import scala.jdk.CollectionConverters.MapHasAsJava
@@ -9,13 +12,13 @@ object AvroSchemeDownloader {
 
     val subject: util.ArrayList[DownloadSubject] = new util.ArrayList[DownloadSubject]
 
-    subject.add(new DownloadSubject(topicName, "", version, false, null))
+    subject.add(new DownloadSubject(topicName, "", version, false , null))
 
     val dwnld = new DownloadTaskAction(
       RegistryClientWrapper.INSTANCE.client(url, auth, Map.empty[String, String].asJava),
       new File("resources"),
       subject,
-      true,
+      true
     )
 
     dwnld.run
