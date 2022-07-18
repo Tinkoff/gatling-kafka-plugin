da55335ffeec807523fd28c63016e7e2ab07f0b5
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-07-18 12:20:23 +0200

    Update kafka-clients, kafka-streams-scala to 3.2.0 (#57)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: John <jigarkhwar88@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   private object Versions {
-    val kafka   = "7.1.1-ce"
+    val kafka   = "3.2.0"
     val gatling = "3.7.6"
     val avro4s  = "4.0.13"
     val avro    = "1.11.0"
