280b3119f5678fe9fb8671aa0061a527f68c984f
a.ugodnikov <a.ugodnikov@tinkoff.ru>
2023-02-02 10:41:39 +0300

    feat: funcs for avro serde

--- src/main/java/ru/tinkoff/gatling/kafka/javaapi/request/builder/RROutTopicStep.java
+++ src/main/java/ru/tinkoff/gatling/kafka/javaapi/request/builder/RROutTopicStep.java
@@ -2,7 +2,7 @@ package ru.tinkoff.gatling.kafka.javaapi.request.builder;
 
 import org.apache.kafka.common.header.Headers;
 import org.apache.kafka.common.header.internals.RecordHeaders;
-import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.*;
 import ru.tinkoff.gatling.kafka.request.builder.KafkaRequestBuilderBase;
 import scala.reflect.ClassTag;
 
@@ -46,6 +46,36 @@ public class RROutTopicStep {
                         Serdes.serdeFrom(keyClass),
                         ClassTag.apply(keyClass),
                         Serdes.serdeFrom(payloadClass),
+                        ClassTag.apply(payloadClass)
+                ));
+    }
+
+    public <K, V> RequestReplyBuilder<?, ?> send(K key, V payload, Class<K> keyClass, Class<V> payloadClass, Serializer<V> ser, Deserializer<V> de) {
+        return new RequestReplyBuilder<K, V>(KafkaRequestBuilderBase.apply(toStringExpression(this.requestName)).requestReply()
+                .requestTopic(toStringExpression(this.inputTopic))
+                .replyTopic(toStringExpression(this.outputTopic))
+                .send(
+                        toStaticValueExpression(key),
+                        toStaticValueExpression(payload),
+                        toStaticValueExpression(new RecordHeaders()),
+                        Serdes.serdeFrom(keyClass),
+                        ClassTag.apply(keyClass),
+                        Serdes.serdeFrom(ser, de),
+                        ClassTag.apply(payloadClass)
+                ));
+    }
+
+    public <K, V> RequestReplyBuilder<?, ?> send(K key, V payload, Headers headers, Class<K> keyClass, Class<V> payloadClass, Serializer<V> ser, Deserializer<V> de) {
+        return new RequestReplyBuilder<K, V>(KafkaRequestBuilderBase.apply(toStringExpression(this.requestName)).requestReply()
+                .requestTopic(toStringExpression(this.inputTopic))
+                .replyTopic(toStringExpression(this.outputTopic))
+                .send(
+                        toStaticValueExpression(key),
+                        toStaticValueExpression(payload),
+                        toStaticValueExpression(headers),
+                        Serdes.serdeFrom(keyClass),
+                        ClassTag.apply(keyClass),
+                        Serdes.serdeFrom(ser, de),
                         ClassTag.apply(payloadClass)
                 ));
     }
