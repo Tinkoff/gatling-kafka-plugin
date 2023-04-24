d9a57bbd018a44b5a4c2dcfd6bb16682aa5c0ee8
i.akhaltsev <i.akhaltsev@tinkoff.ru>
2023-04-24 15:21:32 +0400

    fix: el is not working

--- src/main/java/ru/tinkoff/gatling/kafka/javaapi/request/builder/KafkaRequestBuilderBase.java
+++ src/main/java/ru/tinkoff/gatling/kafka/javaapi/request/builder/KafkaRequestBuilderBase.java
@@ -1,43 +1,58 @@
 package ru.tinkoff.gatling.kafka.javaapi.request.builder;
 
+import io.gatling.commons.validation.Validation;
+import io.gatling.core.session.Session;
 import org.apache.kafka.common.header.Headers;
 import org.apache.kafka.common.header.internals.RecordHeaders;
+import scala.Function1;
 
 import static io.gatling.javaapi.core.internal.Expressions.*;
+import static io.gatling.javaapi.core.internal.Expressions.toStaticValueExpression;
 
 public class KafkaRequestBuilderBase {
 
     private final ru.tinkoff.gatling.kafka.request.builder.KafkaRequestBuilderBase wrapped;
     private final String requestName;
 
-    public KafkaRequestBuilderBase(ru.tinkoff.gatling.kafka.request.builder.KafkaRequestBuilderBase wrapped, String requestName){
+    private <T> Function1<Session, Validation<T>> calculateExpression(T obj) {
+        Function1<Session, Validation<T>> expression;
+
+        if (obj instanceof String || obj.getClass().isPrimitive() || obj instanceof CharSequence || obj instanceof byte[]) {
+            expression = toExpression(obj.toString(), obj.getClass());
+        } else {
+            expression = toStaticValueExpression(obj);
+        }
+        return expression;
+    }
+
+    public KafkaRequestBuilderBase(ru.tinkoff.gatling.kafka.request.builder.KafkaRequestBuilderBase wrapped, String requestName) {
         this.wrapped = wrapped;
         this.requestName = requestName;
     }
 
-    public <K,V> RequestBuilder<?, ?> send(K key, V payload) {
+    public <K, V> RequestBuilder<?, ?> send(K key, V payload) {
         return new RequestBuilder<>(
                 wrapped.send(
-                        toStaticValueExpression(key),
-                        toStaticValueExpression(payload),
+                        calculateExpression(key),
+                        calculateExpression(payload),
                         toStaticValueExpression(new RecordHeaders()),
                         ru.tinkoff.gatling.kafka.request.builder.Sender.noSchemaSender()
                 ));
     }
 
-    public <K,V> RequestBuilder<?, ?> send(K key, V payload, Headers headers) {
+    public <K, V> RequestBuilder<?, ?> send(K key, V payload, Headers headers) {
         return new RequestBuilder<>(
                 wrapped.send(
-                        toStaticValueExpression(key),
-                        toStaticValueExpression(payload),
+                        calculateExpression(key),
+                        calculateExpression(payload),
                         toStaticValueExpression(headers),
                         ru.tinkoff.gatling.kafka.request.builder.Sender.noSchemaSender()
                 ));
     }
 
-    public <V> RequestBuilder<Object, ?> send(V payload) {
+    public <V> RequestBuilder<?, ?> send(V payload) {
         return new RequestBuilder<>(wrapped.send(
-                toStaticValueExpression(payload),
+                calculateExpression(payload),
                 ru.tinkoff.gatling.kafka.request.builder.Sender.noSchemaSender()));
     }
 
